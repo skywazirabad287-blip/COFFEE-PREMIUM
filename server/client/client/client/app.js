f56c0de6058c84df32f7342b58a356dbb0174381
skywazirabad287-blip <skywazirabad287@gmail.com>
2026-05-04 13:30:46 +0500

    Delete server directory

--- server/client/client/client/app.js
+++
@@ -1,8 +0,0 @@
-fetch('/api/menu')
-.then(res => res.json())
-.then(data => {
-  const menu = document.getElementById('menu');
-  data.forEach(item => {
-    menu.innerHTML += `<div class="card">${item.name} - $${item.price}</div>`;
-  });
-});
